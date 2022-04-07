a003a1dab6ee1bb1f0977d307b27a9c8c1e1ff35
kirill <kirillrocker@gmail.com>
2022-04-07 20:49:18 +0300

    modify_js

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-let numberOfFilms = prompt('Сколько фильмов вы посмотрели?');
+let numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
 
 
 const personalMovieDb = {
     count : numberOfFilms,
-    movies : {
-    },
+    movies : {},
     actors : {},
     genres : [],
     privat : false,
 }
-let a = prompt('Один из последних фильмов?')
-let b = prompt('Оценка')
+    for (let i = 0; i < 2; i++) {
+            let a = prompt('Один из последних фильмов?');
+            let b = prompt('Оценка');
+            if (a != null && b!= null && a!='' && b!='' && a.length<50) {
+                personalMovieDb.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log ('error');
+                i--;
+            }
+        }
 
-let named = prompt('Имя')
-let surn = prompt('Фамилия')
-
-personalMovieDb.actors[named] = surn; 
-
-personalMovieDb.movies[a] = b;
-
-console.log(personalMovieDb);
+        if(personalMovieDb.count < 10) {
+            console.log('Просмотрено довольно мало фильмов')
+        } else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+            console.log ('Вы - классический зритель')
+        } else if (personalMovieDb.count >= 30) {
+            console.log('Вы - киноман.')
+        }
+        else {
+            console.log('ERROR')
+        }
+console.log(personalMovieDb)
